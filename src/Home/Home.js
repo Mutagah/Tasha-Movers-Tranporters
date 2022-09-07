32d885826aa4c4882cb5b92a2622465d162c1a28
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-08 02:39:32 +0300

    added styling and data to the registerHouse component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,27 +2,38 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel"
 function Home(){
     return (
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://images.unsplash.com/photo-1591768793355-74d04bb6608f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" class="d-block w-100" alt="Truck Image"/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.unsplash.com/photo-1624339024061-b435d9261c1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" class="d-block w-100" alt="Pick-up "/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.unsplash.com/photo-1591706515036-cb0f48dc5e62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="d-block w-100" alt="Van image"/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-inline h-50 w-100"
+                 src="https://images.unsplash.com/photo-1591768793355-74d04bb6608f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80/800x400?text=First slide&bg=373940"
+                  alt="Truck Image"
+                />
+                <Carousel.Caption>
+                  <h1>We are reliable</h1>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 "
+                  src="https://images.unsplash.com/photo-1624339024061-b435d9261c1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80/800x400?text=Second slide&bg=282c34"
+                  alt="Pick-up image"
+                />
+                <Carousel.Caption>
+                  <h1>We are transparent</h1>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1591706515036-cb0f48dc5e62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80/800x400?text=Third slide&bg=20232a"
+                  alt="Van image"
+                />
+                <Carousel.Caption>
+                  <h1>We are accountable</h1>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           );
         }
 export default Home;
--- src/RegisterTask/RegisterTask.js
+++ src/RegisterTask/RegisterTask.js
@@ -1,9 +1,27 @@
 import React from "react"
 import "./RegisterTask.css"
+import useFetch from "../customHooks/useFetch"
 function RegisterTask(){
+    const {data} = useFetch("http://localhost:9292/vehicles")
+    console.log(data)
+    const displayVehicles = data.map((element)=>
+        {
+            return( 
+        <div class="col-md-3">
+            <div class="card p-2">
+                <div class="text-right"> <small>{element.vehicle_type}</small> </div>
+                <div class="text-center mt-2 p-3"> <img src={element.vehicle_image_url} width="250" height="200"/> <span class="d-block font-weight-bold">{element.vehicle_type}</span>
+                    <hr/> <span>{element.registration_no}</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i>  </div>
+                    <div class="d-flex justify-content-between mt-3"> <span><button classname="btn btn-dark btn-lg mt-4">Book me</button></span> </div>
+                </div>
+            </div>
+           
+        </div>
+    )})
     return(
         <div className="mt-3 mb-5">
-            <h5>Please fill in your taks details here</h5>
+            <h3 className="mt-4">Please fill in your taks details here</h3>
         <form >
             <div className="row g-2">
                 <div className="col-12 col-md-6 col-lg-6">
@@ -13,7 +31,7 @@ function RegisterTask(){
 
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter Task description:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Task Description" />
+                    <input type="text" className="form-control form-control-lg" placeholder="Task Description(include the weight)" />
                 </div>
 
                 <div className="col-12 col-md-6 col-lg-6">
@@ -23,7 +41,7 @@ function RegisterTask(){
 
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter task destination:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Task destination(include the weight)" />
+                    <input type="text" className="form-control form-control-lg" placeholder="Task destination" />
                 </div>
 
                 <div className="col-12 col-md-6 col-lg-6">
@@ -37,8 +55,13 @@ function RegisterTask(){
                 </div>
             </div>
         </form>
-        <h1 className="mt-4"> Please Book your preffered means on vehicle</h1>
+        <h3 className="mt-4"> Please Book your preffered means on vehicle</h3>
+        <input type="text" placeholder="Enter car type:" className="form-control form-control-lg"/>
+        <div className="row mt-2 g-1">
+        {displayVehicles}
+        </div>
     </div>
     )
+    return 
 }
 export default RegisterTask
