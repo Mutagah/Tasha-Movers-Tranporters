83a1a8936160078a5a7bc7f6823d7bfeaa2bebbe
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-08 03:49:55 +0300

    implemented the handle submit of the register task component

--- src/RegisterTask/RegisterTask.js
+++ src/RegisterTask/RegisterTask.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React,{useState} from "react"
 import "./RegisterTask.css"
 import useFetch from "../customHooks/useFetch"
 function RegisterTask(){
     const {data} = useFetch("http://localhost:9292/vehicles")
-    console.log(data)
+    const [formdata, setformdata] = useState({
+        company_name:"",
+        task_description:"",
+        task_location_departure:"",
+        task_destination:"",
+        estimated_distance_in_km:""
+    })
     const displayVehicles = data.map((element)=>
         {
             return( 
@@ -19,35 +25,54 @@ function RegisterTask(){
            
         </div>
     )})
+    function handleChange(event)
+    {
+        setformdata({...formdata,[event.target.name]:event.target.value})
+    }
+    console.log(formdata)
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch("http://localhost:9292/tasks",{
+            method : "POST",
+            headers :
+            {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(formdata)
+        })
+
+    }
     return(
         <div className="mt-3 mb-5">
             <h3 className="mt-4">Please fill in your taks details here</h3>
-        <form >
+        <form onSubmit={handleSubmit}>
             <div className="row g-2">
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter Company name:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Company name"/>
+                    <input type="text" className="form-control form-control-lg" placeholder="Company name" onChange={handleChange} name="company_name" value={formdata.company_name}/>
                 </div>
 
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter Task description:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Task Description(include the weight)" />
+                    <input type="text" className="form-control form-control-lg" placeholder="Task Description(include the weight)" onChange={handleChange} name="task_description" value={formdata.task_description}/>
                 </div>
 
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter task Location departure:</label>
-                    <input type="text"  className="form-control form-control-lg" placeholder="Location Departure" />
-                </div>
-
-                <div className="col-12 col-md-6 col-lg-6">
-                    <label>Enter task destination:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Task destination" />
+                    <input type="text"  className="form-control form-control-lg" placeholder="Location Departure" onChange={handleChange} name="task_location_departure" value={formdata.task_location_departure} />
                 </div>
 
                 <div className="col-12 col-md-6 col-lg-6">
                     <label>Enter estimated distance:</label>
-                    <input type="text" className="form-control form-control-lg" placeholder="Estimated distance in km" />
+                    <input type="text" className="form-control form-control-lg" placeholder="Estimated distance in km" onChange={handleChange} value={formdata.estimated_distance_in_km} name="estimated_distance_in_km"/>
                 </div>
+
+                <div className="col-12 col-md-6 col-lg-6">
+                    <label>Enter task destination:</label>
+                    <input type="text" className="form-control form-control-lg" placeholder="Task destination" onChange={handleChange} value={formdata.task_destination} name="task_destination"/>
+                </div>
+
+                
                 <div className="col-12 col-md-6 col-lg-4 text-end">
                     <button type="submit" className="btn btn-dark btn-lg mt-4">
                         SAVE TASK
@@ -62,6 +87,5 @@ function RegisterTask(){
         </div>
     </div>
     )
-    return 
 }
 export default RegisterTask
