55e82045820cb2d90e0c7348b1352e92c0048633
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-09 14:46:20 +0300

    added images to the home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Home.css"
 import Carousel from "react-bootstrap/Carousel"
 function Home(){
-    return (
+    return (<div>
            <div className="mt-3 container">
             <div className="row">
-              <div className="col-md-8 col-lg-10 offset-md-2 offset-lg-1">
+              <div className="col-md- col-lg-10 offset-md-2 offset-lg-1">
               <Carousel>
               <Carousel.Item>
                 <img
-                  className="d-inline h-50 w-100"
+                  className="d-inline w-100"
                  src="https://images.unsplash.com/photo-1591768793355-74d04bb6608f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80/800x400?text=First slide&bg=373940"
                   alt="Truck Image"
                 />
@@ -40,6 +41,45 @@ function Home(){
               </div>
             </div>
            </div>
+            <div className="stylingpic">
+           <div class="stylingpic container-fluid grid gap-10 px-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div class="row">
+              <div class="col-sm">
+                  <div className="card carding" style={{width: "18rem"}}>
+                  <img src="https://image.shutterstock.com/image-photo/truck-goes-on-highway-evening-600w-1551201551.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h3 className="card-text">We are realiable</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm cardsZetu">
+              <div className="card carding" style={{width: "18rem"}}>
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Ford_F-150_crew_cab_--_05-28-2011.jpg/1200px-Ford_F-150_crew_cab_--_05-28-2011.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h3 className="card-text">Guaranteed good services</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm cardsZetu">
+              <div className="card carding" style={{width: "18rem"}}>
+                  <img src="https://professionalpickup.com/wp-content/uploads/2022/03/Audi-Pickup-Truck-Concept-by-Kleber-Silva-2022-Professional-Pickup-01.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h3 className="card-text">We are affordable</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm">
+              <div className="card carding" style={{width: "18rem"}}>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgRiZXdm3FPRZEdCd168bj8fu-dWd0fw03Sg&usqp=CAU" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h3 className="card-text">We are accountable</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+         </div>
           );
         }
 export default Home;
