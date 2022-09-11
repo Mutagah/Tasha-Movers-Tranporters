861232a78fda06fcbd99c1e8f9cc4ce9506f8d69
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-11 19:16:43 +0300

    shifted the logic handling to the parent file app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
 import './App.css';
 import Home from "./Home/Home"
+import useFetch from "./useFetch"
 import Navbar from "./Navbar/Navbar"
+import React,{useState} from "react"
 import ViewTask from './ViewTask/ViewTask';
 import {Route,Switch} from "react-router-dom"
+import { useHistory } from 'react-router-dom'
 import RegisterTask from './RegisterTask/RegisterTask';
-import RegisterDrivers from './RegisterDrivers/RegisterDrivers';
-
+import AssignVehicle from './AssignVehicle/AssignVehicle';
 function App() {
+  const history = useHistory()
+  const [driverId, setDriverId] = useState(0)
+  const [vehicleId, setVehicleId] = useState(0)
+  const {data, setdata} = useFetch("http://localhost:9292/drivers")
+  const [searchCriteria, setSearchCriteria] = useState("")
+    function handleSearchCriteria(event)
+    {
+        setSearchCriteria(event.target.value)
+    }
+
+    function handleDriverAssigment(driverIdNumber){
+      setDriverId(driverIdNumber)   
+    const filteredDrivers = data.filter((driver)=>
+    {
+      return(driver.id !== driverIdNumber)
+    })  
+    setdata(filteredDrivers)
+    }
+
+    const [formdata, setformdata] = useState({
+      task_description:"",
+      departure_location:"",
+      destination_location:"",
+      estimated_distance_in_km:""
+  })
+  console.log(driverId)
+    const filteredDisplay = data.filter((driver)=>
+         driver.vehicle_type_licensed.toLowerCase().includes(searchCriteria.toLowerCase())
+    )  
+    function handleChange(event)
+    {
+        setformdata({...formdata,[event.target.name]:event.target.value})
+    }
+    console.log(formdata)
+    function handleTaskSubmission(event){
+        event.preventDefault()
+        fetch("http://localhost:9292/tasks",{
+            method : "POST",
+            headers :
+            {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({...formdata,driver_id: driverId,vehicle_id: vehicleId})
+        })
+        setformdata({
+            task_description:"",
+            departure_location:"",
+            destination_location:"",
+            estimated_distance_in_km:"",
+        })
+        setDriverId(0)
+        setVehicleId(0)
+        history.push("/viewtask")
+    }
+    // console.log(vehicleId)
+
   return (
     <div>
       <Navbar/>
@@ -15,10 +73,10 @@ function App() {
           <Home/>
         </Route>
         <Route exact path ="/registertask">
-          <RegisterTask/>
+          <RegisterTask filteredDisplay={filteredDisplay} formdata={formdata} handleChange={handleChange} handleDriverAssigment={handleDriverAssigment} handleSearchCriteria={handleSearchCriteria}/>
         </Route>
-        <Route exact path ="/registerdriver">
-          <RegisterDrivers/>
+        <Route exact path ="/assignvehicle">
+          <AssignVehicle setVehicleId={setVehicleId} handleTaskSubmission={handleTaskSubmission}/>
         </Route>
         <Route exact path ="/viewtask">
           <ViewTask/>
