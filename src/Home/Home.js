6ec79c703dea111fd9041beaef21a720dc06bf41
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-11 19:22:36 +0300

    Added more images to the home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -42,8 +42,8 @@ function Home(){
             </div>
            </div>
             <div className="stylingpic">
-           <div class="stylingpic container-fluid grid gap-10 px-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <div class="row">
+           <div className="stylingpic container-fluid grid gap-10 px-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="row">
               <div class="col-sm">
                   <div className="card carding" style={{width: "18rem"}}>
                   <img src="https://image.shutterstock.com/image-photo/truck-goes-on-highway-evening-600w-1551201551.jpg" className="card-img-top" alt="..."/>
@@ -52,7 +52,7 @@ function Home(){
                   </div>
                 </div>
               </div>
-              <div class="col-sm cardsZetu">
+              <div className="col-sm cardsZetu">
               <div className="card carding" style={{width: "18rem"}}>
                   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Ford_F-150_crew_cab_--_05-28-2011.jpg/1200px-Ford_F-150_crew_cab_--_05-28-2011.jpg" className="card-img-top" alt="..."/>
                   <div className="card-body">
@@ -60,7 +60,7 @@ function Home(){
                   </div>
                 </div>
               </div>
-              <div class="col-sm cardsZetu">
+              <div className="col-sm cardsZetu">
               <div className="card carding" style={{width: "18rem"}}>
                   <img src="https://professionalpickup.com/wp-content/uploads/2022/03/Audi-Pickup-Truck-Concept-by-Kleber-Silva-2022-Professional-Pickup-01.jpg" className="card-img-top" alt="..."/>
                   <div className="card-body">
@@ -68,7 +68,7 @@ function Home(){
                   </div>
                 </div>
               </div>
-              <div class="col-sm">
+              <div className="col-sm">
               <div className="card carding" style={{width: "18rem"}}>
                   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgRiZXdm3FPRZEdCd168bj8fu-dWd0fw03Sg&usqp=CAU" className="card-img-top" alt="..."/>
                   <div className="card-body">
