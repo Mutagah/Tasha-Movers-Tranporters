6eae6e4b1c24ef3fd11196095150496ea6ee005b
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-11 19:20:13 +0300

    shifted the useFetch.js component to src folder

---
+++ src/useFetch.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+function useFetch (url){
+     const [data, setdata] = useState([]);
+
+    useEffect(()=> {
+        fetch(url)
+        .then((resp)=> resp.json())
+        .then(data => setdata(data))
+    },[])
+    return {data, setdata}
+}
+export default useFetch
